1c230c8d9a2e49133157aad449bc2fde20fbd719
nkhaja <nbl.khaja@gmail.com>
2017-01-25 14:26:32 -0800

    first commit

--- bases.py
+++ bases.py
@@ -1,6 +1,15 @@
 #!python
 
 import string
+import math
+
+# def mapLetters():
+#     alphabet = {}
+#     alpha = "abcdefghijklmnopqrstuvwxyz"
+#     for a, index in alpha.enumerate():
+#         alphabet[a] = index + 10
+#     return alphabet
+
 
 
 def decode(str_num, base):
@@ -10,7 +19,21 @@ def decode(str_num, base):
     base -- base of given number
     """
     assert 2 <= base <= 36
-    # TODO: Decode number
+
+    tally = 0
+    digits = len(str_num)
+    power = digits - 1
+    for i in range(digits):
+        num = None
+        digit = str_num[i]
+        if digit.isdigit():
+            num = int(digit)
+        else:
+            num = ord(digit) - 97 + 10
+        tally = tally + (base**power)*num
+        power = power - 1
+    return tally
+
 
 def encode(num, base):
     """
@@ -19,7 +42,24 @@ def encode(num, base):
     base -- base to convert to
     """
     assert 2 <= base <= 36
-    # TODO: Encode number
+
+    num_digits = math.ceil(log(num,base))
+    remainder = None
+    residual = num
+    output = []
+    for i in range(num_digits, - 1):
+        baseToPower = base**i
+        if baseToPower > num:
+            output.append("0")
+        else:
+            remainder = math.floor(residual/baseToPower)
+            residual = residual - baseToPower
+            output.append(str(remainder))
+
+
+    numString = "".join(output)
+
+
 
 def convert(str_num, base1, base2):
     """
@@ -27,6 +67,8 @@ def convert(str_num, base1, base2):
     """
     assert 2 <= base1 <= 36
     assert 2 <= base2 <= 36
+
+
     # TODO: Convert number
 
 
@@ -44,4 +86,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    print decode('101',2)
